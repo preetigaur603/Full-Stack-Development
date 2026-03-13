400cccd09e044fe8b1dedf136cefc6882ccd2518
preetigaur603 <preetigaur603@gmail.com>
2026-03-13 18:56:11 +0530

    Refactor book form submission and rendering logic

--- javascript.js
+++ javascript.js
@@ -1,84 +1,73 @@
 const bookForm = document.getElementById("BookForm");
-const bookList = document.getElementById("BookList");
+const bookList = document.getElementById("book-list");
 const submitBtn = document.getElementById("submit-btn");
+
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+const pub_date = document.getElementById("pub_date");
+const genre = document.getElementById("genre");
 
 let books = [];
 let editingIndex = -1;
 
-const calculateAge = (date) => {
-  
-  const d = new Date(date);
-  const t = new Date();
-  let age = t.getFullYear() - d.getFullYear();
-  if (t.getMonth() < d.getMonth() ||
-    (t.getMonth() === d.getMonth() && t.getDate() < d.getDate())) age--;
-  return age <= 0 ? "New" : `${age} yrs`;
+bookForm.addEventListener("submit", function(e) {
+e.preventDefault();
+
+const data = {
+title: title.value,
+author: author.value,
+isbn: isbn.value,
+publication_date: pub_date.value,
+genre: genre.value
 };
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+if(editingIndex === -1){
+books.push(data);
+}else{
+books[editingIndex] = data;
+editingIndex = -1;
+submitBtn.textContent = "Add Book";
+}
 
-
-  const data = {
-    title: title.value,
-    author: author.value,
-    isbn: isbn.value,
-    publication_date: publication_date.value,
-    genre: genre.value
-  };
-
-
-  if (editingIndex >= 0) {
-    books[editingIndex] = data;
-    editingIndex = -1;
-    submitBtn.textContent = "Add Book";
-  } else {
-    books.push(data);
-  }
-
-
-  bookForm.reset();
-  renderBooks();
+bookForm.reset();
+renderBooks();
 });
 
+function renderBooks(){
+bookList.innerHTML = "";
 
-const renderBooks = () => {
-  bookList.innerHTML = "";
-  books.forEach((b, i) => {
-    bookList.innerHTML += `
-      <tr class="border-b">
-        <td class="p-2">${b.title}</td>
-        <td>${b.author}</td>
-        <td>${b.isbn}</td>
-        <td>${b.publication_date}</td>
-        <td>${calculateAge(b.publication_date)}</td>
-        <td>${b.genre}</td>
-        <td class="space-x-2">
-          <button onclick="editBook(${i})" class="text-green-600">Edit</button>
-          <button onclick="deleteBook(${i})" class="text-red-600">Delete</button>
-        </td>
-      </tr>`;
-  });
-};
+books.forEach((b,i)=>{
+bookList.innerHTML += `
+<tr class="border">
+<td class="p-2">${b.title}</td>
+<td>${b.author}</td>
+<td>${b.isbn}</td>
+<td>${b.publication_date}</td>
+<td>${b.genre}</td>
+<td>
+<button onclick="editBook(${i})" class="text-green-600 mr-2">Edit</button>
+<button onclick="deleteBook(${i})" class="text-red-600">Delete</button>
+</td>
+</tr>
+`;
+});
+}
 
+function editBook(i){
+const b = books[i];
 
-window.editBook = (i) => {
-  const b = books[i];
-  title.value = b.title;
-  author.value = b.author;
-  isbn.value = b.isbn;
-  publication_date.value = b.publication_date;
-  genre.value = b.genre;
-  editingIndex = i;
-  submitBtn.textContent = "Update Book";
-};
+title.value = b.title;
+author.value = b.author;
+isbn.value = b.isbn;
+pub_date.value = b.publication_date;
+genre.value = b.genre;
 
+editingIndex = i;
+submitBtn.textContent = "Update Book";
+}
 
-window.deleteBook = (i) => {
-  books.splice(i, 1);
-  renderBooks();
-};
-
-
-
-
+function deleteBook(i){
+books.splice(i,1);
+renderBooks();
+}
